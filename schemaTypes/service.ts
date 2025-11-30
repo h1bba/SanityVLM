5206b7eea6f9d849a01d6b2f2b1b4ad3498edc06
Ufuk <hibbayt@gmail.com>
2025-11-30 23:31:38 +0100

    update service

--- schemaTypes/service.ts
+++ schemaTypes/service.ts
@@ -11,20 +11,38 @@ export default defineType({
       type: 'string',
       validation: (Rule) => Rule.required().min(2).max(80),
     }),
+
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+        slugify: (input: string) =>
+          input
+            .toLowerCase()
+            .trim()
+            .replace(/\s+/g, '-') // spaties → -
+            .replace(/[^a-z0-9-]/g, '') // rare tekens weg
+            .slice(0, 96),
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+
     defineField({
       name: 'description',
       title: 'Omschrijving voor de diensten pagina',
       type: 'array',
       of: [{type: 'block'}],
-      validation: (Rule) => Rule.required().min(10),
+      validation: (Rule) => Rule.required().min(1),
     }),
     defineField({
       name: 'descriptionPage',
       title: 'Omschrijving voor de dienst zelf',
       type: 'array',
       of: [{type: 'block'}],
-
-      validation: (Rule) => Rule.required().min(10),
+      validation: (Rule) => Rule.required().min(1),
     }),
     defineField({
       name: 'photoUrl',
